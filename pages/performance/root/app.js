9d26201d5eac3e246dca047b77e88ffcdce68bb2
Michael Morran <michaelmorran@mmorran_mbp.itp.tsoa.nyu.edu>
2021-06-13 10:57:49 -0400

    active performance functionality

--- pages/performance/root/app.js
+++ pages/performance/root/app.js
@@ -1,5 +1,44 @@
 console.log('hello world');
 
+let audienceSocket = io('/audience');
+let activePerformers = [];
+
+audienceSocket.on('activePerformers', (data) => {
+    for (i = 0; i < data.length; i++) {
+        activePerformers.push(data);
+    }
+    console.log(activePerformers);
+});
+
+audienceSocket.on('getPresence', (data) => {
+    activePerformers.push(data);
+    console.log(activePerformers);
+});
+
+audienceSocket.on('performerDisconnect', (data) => {
+    // console.log(data);
+    for (i = 0; i < activePerformers.length; i++) {
+        if (activePerformers[i].id === data) {
+            activePerformers.splice(i, 1);
+            // console.log(data + "disconnected");
+        }
+    }
+});
+
+window.addEventListener('load', () => {
+    document.getElementById('audienceNav').addEventListener('click', () => {
+        if (activePerformers.length < 1) {
+            document.getElementById('audienceNav').innerHTML = "No live performances";
+            setTimeout(() => {
+                document.getElementById('audienceNav').innerHTML = "AN AUDIENCE MEMBER";
+            }, 3000);
+        } else {
+            document.getElementById('audienceNav').href = '/performance/audience';
+            window.location.href = document.getElementById('audienceNav').href
+        }
+    })
+})
+
 // window.addEventListener('load', () => {
 //     document.getElementById('button1').addEventListener('click', () => {
 //         console.log('button 1 clicked');
